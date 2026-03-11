6286de4808f598f7626ea20a11a6d85cf4839cfc
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2026-03-11 07:32:05 -0600

    add wide string support to IgnoreCaseComparer

--- source/IgnoreCaseComparer.h
+++ source/IgnoreCaseComparer.h
@@ -6,4 +6,8 @@ struct IgnoreCaseComparer {
     bool operator()(const std::string &str, const std::string &str2) const noexcept {
         return _stricmp(str.c_str(), str2.c_str()) < 0;
     }
+
+    bool operator()(const std::wstring &str, const std::wstring &str2) const noexcept {
+        return _wcsicmp(str.c_str(), str2.c_str()) < 0;
+    }
 };
