00f1f4ee3805b97293c6c06d42974d2e020fe7ad
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2026-08-11 13:07:59 -0600

    update IgnoreCaseComparer

--- source/IgnoreCaseComparer.h
+++ source/IgnoreCaseComparer.h
@@ -1,13 +1,34 @@
 #pragma once
 #include <string>
 #include <string.h>
+#include <type_traits>
 
 struct IgnoreCaseComparer {
-    bool operator()(const std::string &str, const std::string &str2) const noexcept {
-        return _stricmp(str.c_str(), str2.c_str()) < 0;
+	using is_transparent = void;
+
+	template <typename T, typename T2>
+    bool operator()(T str, T2 str2) const noexcept {
+		if constexpr (std::is_same_v<decltype(c_str(str)), const wchar_t*>) {
+			return _wcsicmp(c_str(str), c_str(str2)) < 0;
+		} else {
+			return _stricmp(c_str(str), c_str(str2)) < 0;
+		}
     }
 
-    bool operator()(const std::wstring &str, const std::wstring &str2) const noexcept {
-        return _wcsicmp(str.c_str(), str2.c_str()) < 0;
-    }
+	private:
+	static const char* c_str(const std::string &str) noexcept {
+		return str.c_str();
+	}
+
+	static const char* c_str(const char* str) noexcept {
+		return str;
+	}
+
+	static const wchar_t* c_str(const std::wstring &str) noexcept {
+		return str.c_str();
+	}
+
+	static const wchar_t* c_str(const wchar_t* str) noexcept {
+		return str;
+	}
 };
